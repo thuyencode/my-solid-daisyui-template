57a81452b34f872f093da8161c0847aa2d509514
thuyencode <thuyencode@proton.me>
2024-01-06 21:22:52 +0700

    Styling

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -6,6 +6,7 @@ const config = {
   useTabs: false,
   semi: false,
   singleQuote: true,
+  jsxSingleQuote: true,
   trailingComma: 'none',
   plugins: ['prettier-plugin-tailwindcss']
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { createSignal, type Component } from 'solid-js'
-import './App.css'
 import solidLogo from './assets/solid.svg'
 // eslint-disable-next-line import/no-absolute-path
 import viteLogo from '/vite.svg'
@@ -9,26 +8,32 @@ const App: Component = () => {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} class="logo" alt="Vite logo" />
+      <div class='flex items-center justify-center gap-10'>
+        <a href='https://vitejs.dev' target='_blank'>
+          <img class='logo h-24 w-auto' src={viteLogo} alt='Vite logo' />
         </a>
-        <a href="https://solidjs.com" target="_blank">
-          <img src={solidLogo} class="logo solid" alt="Solid logo" />
+        <a href='https://solidjs.com' target='_blank'>
+          <img
+            class='logo solid h-24 w-auto'
+            src={solidLogo}
+            alt='Solid logo'
+          />
         </a>
       </div>
-      <h1>Vite + Solid</h1>
-      <div class="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count()}
+      <h1 class='text-5xl font-bold'>Vite + Solid</h1>
+      <div class='space-y-5'>
+        <button
+          class='btn btn-primary text-base'
+          role='button'
+          onClick={() => setCount((count) => ++count)}
+        >
+          Count is {count()}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+        <p class='text-sm'>
+          Edit <code class='kbd kbd-sm'>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p class="read-the-docs">
-        Click on the Vite and Solid logos to learn more
-      </p>
+      <p class='font-medium'>Click on the Vite and Solid logos to learn more</p>
     </>
   )
 }
